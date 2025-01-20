2bb5d409404bcdce4ba2ff270179d154d495ea63
ferrondw <145978195+ferrondw@users.noreply.github.com>
2025-01-20 10:43:05 +0100

    :3

--- index.js
+++ index.js
@@ -109,6 +109,6 @@ app.delete('/api/items/:index', (req, res) => {
     });
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
